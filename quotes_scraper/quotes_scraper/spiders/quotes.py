32f5dc06c4cd759cd0e6316c6afb06057f0784cd
Juan Tovar <jujotomo@gmail.com>
2020-11-07 09:36:10 -0800

    Scraping multiple pages with response.follow

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,6 +3,8 @@ import scrapy
 # Title = //h1/a/text()
 # Quotes = //span[@class="text" and @itemprop="text"]/text()'
 # Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button = //ul[@class="pager"]/li[@class="next"]/a/@href').get()
+
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -10,19 +12,27 @@ class QuotesSpider(scrapy.Spider):
         'http://quotes.toscrape.com/'
     ]
 
+    # Saving JSON automatically
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
+
     def parse(self, response):
-        
+
         title = response.xpath('//h1/a/text()').get()
-        quotes=response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        quotes = response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall()
+        top_ten_tags = response.xpath(
+            '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
 
         yield {
             'title': title,
             'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
-       
-        
 
-
-        
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]/li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse)
